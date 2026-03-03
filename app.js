e907ea5ccabe9e4f42dea5dd23b97b43384161be
tnyamao <29414584+tnyamao@users.noreply.github.com>
2026-03-03 16:57:26 +0900

    Toggle todo done on click

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const form = document.getElementById('todo-form');
 const input = document.getElementById('todo-input');
 const list = document.getElementById('todo-list');
 
+let nextId = 1;
+/** @type {{id:number, text:string, done:boolean} []} **/
 let todos = [];
 
 function escapeHtml(str) {
@@ -17,21 +19,37 @@ function escapeHtml(str) {
 }
 
 function render() {
-    list.innerHTML = todos
-        .map(t => `<li>${escapeHtml(t)}</li>`)
-        .join('');
-}
+    list.innerHTML = todos.map(t => {
+        const doneClass = t.done ? 'is-done' : '';
+        return `<li class="${doneClass}" data-id="${t.id}">${escapeHtml(t.text)}</li>`;
+    }).join('');
+};
 
 form.addEventListener('submit', (e) => {
-    console.log("submit fired");
     e.preventDefault();
-
     const text = input.value.trim();
     if (!text) return;
 
-    todos.push(text);
+    todos.push({ id: nextId++, text, done: false});
     input.value = '';
     input.focus();
+    render();
+});
+
+list.addEventListener('click', (e) => {
+    const li = e.target.closest('li');
+
+    if (!(e.target instanceof Element)) {
+        return;
+    }
+
+    const id = Number(li.dataset.id);
+
+    const t = todos.find(x => x.id === id);
+
+    if (!li) return;
+
+    t.done = !t.done;
     render();
 });
 
